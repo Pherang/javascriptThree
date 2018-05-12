5c5052e1b62368af3cbd4171298e9a4267d5c33b
Pherang <pherang@users.noreply.github.com>
2018-05-12 16:52:50 -0400

    Rewrote the function after understanding symmetrical differences more.

--- symDiffArrays.js
+++ symDiffArrays.js
@@ -2,8 +2,28 @@
 // It could be two or more arrays passed to the function.
 
 function sym(args) {
-  var symmDiff = []
-  return symmDiff
+  var symmDiff = [] 
+  var tempArray = [...arguments]
+  
+  // The way javascript works, calling symDiff = tempArray.reduce
+  // Javascript will start that call and then move onto the next statement
+  // I had return symDiff below before but that resulted in an empty array returning everytime
+  //
+  return symDiff = tempArray.reduce( function (accumulator, currentValue, currentIndex, array) {
+    // Symmetrical difference works by seeing if the next set has elements that the current set doesn't.
+    // If the current set doesn't have an element from the next that element is added to the current set.
+    // This works for subsequent sets of numbers
+    array[currentIndex].forEach( function(element) {
+      if (accumulator.indexOf(element) == -1){
+        console.log('OK', element )
+        accumulator.push(element)
+        console.log(accumulator)
+      }
+    })
+    return accumulator
+  },[]
+  )
 }
 
-console.log(sym([1, 2, 5], [2,3,5], [3,4,5] ))
+var doh = sym([1, 3, 5], [2,3,5], [3,4,5] )
+console.log(doh)
