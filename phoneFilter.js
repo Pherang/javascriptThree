b27ba9f43a9801c71174719bcf4bf639af03866e
Pherang <Pherang@users.noreply.github.com>
2018-05-12 06:45:41 -0400

    Update phoneFilter.js

--- phoneFilter.js
+++ phoneFilter.js
@@ -1,4 +1,9 @@
 function telephoneCheck(str) {
+  
+  // Some of these regex can probably be refactored. Format 1 and two are similar enough to combine..
+  // Format 3 and 4 can also probably be combined.
+  // At the same time though, it's easier for someone else to read the filters if they are separated out
+  
   var phoneFormat1 = /^\d{3}-\d{3}-\d{4}/
   var phoneFormat2 = /^\(\d{3}\)\d{3}-\d{4}/
   var phoneFormat3 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}/
