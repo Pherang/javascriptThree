7853631104b77b1fff14d7b6855153ed834c0645
Pherang <pherang@users.noreply.github.com>
2018-05-12 14:10:26 -0400

    Need to redo, need to understand symmetric difference

--- symDiffArrays.js
+++ symDiffArrays.js
@@ -2,28 +2,8 @@
 // It could be two or more arrays passed to the function.
 
 function sym(args) {
-  
-  // Using spread operator on arguments to create an array
-  var inputArrays = [...arguments]
-  // Flatten the array first and then we can reduce and use another array function to help us find duplicate values.
-  // 
-  var temp = inputArrays.reduce( function(acc, currentVal) {
-    return acc.concat(currentVal)
-  },[]
-  )
-  console.log('flattened array ', temp) 
-  var symmDiff = temp.reduce( function (acc, currentVal, currentIndex, array) {
-    //
-    // If we find one value and then from that index find a second value, that means we have two values and it can't be unique
-    // to the set.
-    if (array.indexOf(currentVal) !== -1 && array.indexOf(currentVal, (array.indexOf(currentVal)+1)) !== -1) {
-        return acc
-    }
-    return acc.concat(currentVal)
-  },[]
-  )
-
+  var symmDiff = []
   return symmDiff
 }
 
-console.log(sym([1, 2, 3], [5, 2, 1, 4]))
+console.log(sym([1, 2, 5], [2,3,5], [3,4,5] ))
